a848e548ec579b179d527fdbe2c096412580c605
jenisha9 <njenisha78@gmail.com>
2024-07-14 22:01:19 +0545

    final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Shop} from "./Pages/Shop";
+import { Shop } from "./Pages/Shop";
 import ShopCategory from "./Pages/ShopCategory";
-import {Product} from "./Pages/Product";
+import Product from "./Pages/Product";
 import LoginSignup from "./Pages/LoginSignup";
-import {Cart} from "./Pages/Cart";
+import Cart from "./Pages/Cart";
 import Footer from "./Components/Footer/Footer";
 import men_banner from "./Components/Assets/banner_mens.png";
 import women_banner from "./Components/Assets/banner_women.png";
